dc389321f1925016cb6104536393c1b6193ec5ad
Ubuntu <ubuntu@ip-172-31-26-136.ec2.internal>
2022-12-21 10:15:43 +0000

    Added user updating logic

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,13 +2,54 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id 
   end
+  
+  
+  def remember(user)
+      user.remember
+      cookies.permanent.encrypted[:user_id] = user.id
+      cookies.permanent[:remember_token] = user.remember_token
+
+  end
+  
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+    
   def current_user 
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id]) # using this for not hitting the database 2much
+    if (user_id = session[:user_id]) # if session with user_is exists
+      @current_user ||= User.find_by(id: user_id) # using this for not hitting the database 2much
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user&.authentificated?(cookies[:remember_token])
+        log_in(user)
+        @current_user = user
+      end
     end
   end
   
   def logged_in?
     !current_user.nil?
   end
+  
+  def current_user?(user) # returns true is the given user is current user
+    user && user == current_user
+  end
+  
+  def redirect_back_or(default) # redirects to stored location (user friendly)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  def store_location # stores the last url wanted to be accessed
+      session[:forwarding_url] = request.original_url if request.get?    
+  end
+  
 end
