a0780a4618bd5a5cd4e464bbb7acc8083d7b05f1
Ubuntu <ubuntu@ip-172-31-26-136.ec2.internal>
2022-12-23 15:56:02 +0000

    Add password reset lofic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { minimum:3, maximum: 50}
@@ -38,6 +38,15 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+  
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
   
   def authentificated?(attribute, token)
     digest = send("#{attribute}_digest")
@@ -47,6 +56,10 @@ class User < ApplicationRecord
   
   def forget
     update_attribute(:remember_digest, nil)
+  end
+  
+  def password_reset_expired?
+      reset_sent_at < 2.hours.ago
   end
   
   private
@@ -59,5 +72,5 @@ class User < ApplicationRecord
       self.activation_token = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
-
+    
 end
